abb06efe26fa5f4e54e2a6d2bd10a5eb2195ff15
Riconeeeee <ometerricone@gmail.com>
2026-03-26 15:58:51 +0800

    added button

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,24 +1,14 @@
-import { IonButton, IonContent, IonFooter, IonHeader, IonTitle, IonToast, IonToolbar, IonPage } from '@ionic/react';
+import { IonButton } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Inline Toast</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonButton id="open-toast" expand="block">
-          Open
-        </IonButton>
-        <p>This toast example uses triggers to automatically open a toast when the button is clicked.</p>
-        <IonToast trigger="open-toast" message="This toast will disappear after 5 seconds" duration={5000}></IonToast>
-      </IonContent>
-    </IonPage>
+    <div id="container">
+       <IonButton expand="block">Block</IonButton>
+      <IonButton expand="full">Full</IonButton>
+    </div>
   );
 };
 
